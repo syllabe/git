8af41fe53509309540f45defebbf6e5ad5f3f3d5
Darek Strojecki <dstroj@wp.pl>
2017-11-08 13:39:05 +0100

    Darek trzecia zmiana

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
 public class Main {
+    
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hej");
+
+        for (int i = 0; i < 100; i++){
+            System.out.println(i);
+        }
     }
 }
