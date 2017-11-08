ddfecea9b95371cf05833a30a12e0abd89449380
Darek Strojecki <dstroj@wp.pl>
2017-11-08 13:32:30 +0100

    Darek druga zmiana

--- src/Test.java
+++ src/Test.java
@@ -2,4 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Cześć");
     }
+    public static void newMetod(String string){
+        System.out.println("Nowy wiersz");
+    }
 }
